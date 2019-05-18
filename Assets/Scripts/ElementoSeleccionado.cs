94c7701654c4432c30d04cc85b65ef8dd22f5324
s.artal <artal.sergio@gmail.com>
2019-05-18 12:53:03 +0200

    Version Final

---
+++ Assets/Scripts/ElementoSeleccionado.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ElementoSeleccionado : MonoBehaviour
+{
+
+    // Que elemento está seleccionado
+    static protected int elementoSeleccionado = 0;
+
+    // Creamos las variablas para almacenar los elementos del juego
+    GameObject fuego, agua, madera;
+
+    // Asignamos a cada variable la ficha con su elemento
+    void Start()
+    {
+        fuego = GameObject.FindGameObjectWithTag("Fuego");
+        agua = GameObject.FindGameObjectWithTag("Agua");
+        madera = GameObject.FindGameObjectWithTag("Madera");
+    }
+
+    // Devuelve el elemnto seleccionado
+    public static int GetElementoSeleccionado()
+    {
+        return elementoSeleccionado;
+    }
+
+    // Asigna el elemento seleccionado
+    public static void SetElementoSeleccionado(int elemento)
+    {
+        elementoSeleccionado = elemento;
+    }
+
+    // Control de la seleccion de elementos
+    private void OnMouseUp()
+    {
+        int numFichasSeleccionadas = FichaSeleccionada.GetTotalFichasSeleccionadas();
+        foreach (int i in FichaSeleccionada.GetFichasSeleccionadas())
+
+        // si no hay fichas del jugador seleccionadas marcaremos y/o desmarcaremos el elemento seleccionado. solo puede haber uno
+        if (numFichasSeleccionadas == 0)
+        {
+            if (elementoSeleccionado == this.GetComponent<Elementos>().idElemento)
+            {
+                CambiaObjeto.PintaBlanco(this.gameObject);
+                ElementoSeleccionado.SetElementoSeleccionado(0);
+            }
+            else
+            {
+                if (elementoSeleccionado == 1) CambiaObjeto.PintaBlanco(fuego.gameObject);
+                if (elementoSeleccionado == 2) CambiaObjeto.PintaBlanco(agua.gameObject);
+                if (elementoSeleccionado == 3) CambiaObjeto.PintaBlanco(madera.gameObject);
+                CambiaObjeto.PintaVerde(this.gameObject);
+                ElementoSeleccionado.SetElementoSeleccionado(this.GetComponent<Elementos>().idElemento);
+            }
+        }
+        else
+        // Si hay fichas del jugador seleccionadas, las pondremos del material correspondiente al elemento y quitaremos todas las selecciones
+        {
+            GameObject[] fichas = null;
+            // Añadimos al array todas las fichas del jugador
+            if (FichaSeleccionada.GetJugador() == 0)
+            {
+                fichas = GameObject.FindGameObjectsWithTag("FichasJugador1");
+            }
+            if (FichaSeleccionada.GetJugador() == 1)
+            {
+                fichas = GameObject.FindGameObjectsWithTag("FichasJugador2");
+            }
+
+            elementoSeleccionado = this.GetComponent<Elementos>().idElemento;
+            foreach (GameObject ficha in fichas)
+            {
+                if (FichaSeleccionada.GetFichasSeleccionadas()[ficha.GetComponent<numFichaJugador>().idFicha-1] == 1)
+                {
+                    // recorremos todas las fichas asignando el elemento y añadiendo a las fichs seleccionadas
+                    if (FichaSeleccionada.GetJugador() == 0)
+                    {
+                        if (ElementoSeleccionado.GetElementoSeleccionado() == 1)
+                        {
+                            CambiaObjeto.AsignaFuegoBlanco(ficha);
+                        }
+
+                        if (ElementoSeleccionado.GetElementoSeleccionado() == 2)
+                        {
+                            CambiaObjeto.AsignaAguaBlanca(ficha);
+                        }
+
+                        if (ElementoSeleccionado.GetElementoSeleccionado() == 3)
+                        {
+                            CambiaObjeto.AsignaMaderaBlanca(ficha);
+                        }
+                    }
+                    if (FichaSeleccionada.GetJugador() == 1)
+                    {
+                        if (ElementoSeleccionado.GetElementoSeleccionado() == 1)
+                        {
+                            CambiaObjeto.AsignaFuegoNegro(ficha);
+                        }
+
+                        if (ElementoSeleccionado.GetElementoSeleccionado() == 2)
+                        {
+                            CambiaObjeto.AsignaAguaNegra(ficha);
+                        }
+
+                        if (ElementoSeleccionado.GetElementoSeleccionado() == 3)
+                        {
+                            CambiaObjeto.AsignaMaderaNegra(ficha);
+                        }
+                    }
+                    FichaSeleccionada.SetFichaJugador(ficha);
+                }
+            }
+            ElementoSeleccionado.SetElementoSeleccionado(0);
+            FichaSeleccionada.InicializaFichasSeleccionadas();
+        }
+    }
+
+ }
---
+++ Assets/Scripts/Salir.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salir : MonoBehaviour
+{
+    // Salir del juego
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
