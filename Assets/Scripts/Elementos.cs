06e68734f40a8200965e7607b5d09579d5b3435c
s.artal <artal.sergio@gmail.com>
2019-05-19 19:14:52 +0200

    limieza

--- Assets/Scripts/Elementos.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Elementos : MonoBehaviour
-{
-    // Identifica el elemento de las fichs de selección de elementos
-    public int idElemento; // 1, 2 o 3 - Fuego, Agua o Madera
-}
